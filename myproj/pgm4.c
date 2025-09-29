659bfb95a78c6feb6c9f39f6fbe3625f026d6073
Nitin <nitinsharma13761870@gmail.com>
2025-09-29 11:33:50 +0530

    Update pgm4.c

--- myproj/pgm4.c
+++ myproj/pgm4.c
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-
+//this is a pgm to print a inverted eq tri(i pulled this)
 int main() {
     int n = 5; // height of the triangle
     for (int i = 0; i < n; i++) {
